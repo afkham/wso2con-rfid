5405775c3224fa95aee1825109eccb199fba6fab
Afkham Azeez <azeez@wso2.com>
2014-06-02 18:52:31 +0530

    Added scripts

--- scripts/start-processes.sh
+++ scripts/start-processes.sh
@@ -4,7 +4,7 @@ logfile="process-check.log"
 
 case "$(ps -ef | grep java | wc -l)" in
 1)  echo "java not running, restarting java:     $(date)" >> $logfile
-    nohup /home/pi/wso2con-rfid/distribution/target/wso2con-rfid-0.0.1-SNAPSHOT/run.sh &
+    /home/pi/wso2con-rfid/distribution/target/wso2con-rfid-0.0.1-SNAPSHOT/run.sh
     ;;
 3)  echo "java running, all OK:     $(date)" >> $logfile
     ;;
@@ -13,7 +13,7 @@ esac
 
 case "$(ps -ef | grep read-rfid.py | wc -l)" in
 1)  echo "python not running, restarting python:     $(date)" >> $logfile
-    nohup sudo python /home/pi/wso2con-rfid/python-scripts/read-rfid.py &
+    sudo python /home/pi/wso2con-rfid/python-scripts/read-rfid.py
     ;;
 3)  echo "python running, all OK:     $(date)" >> $logfile
     ;;
