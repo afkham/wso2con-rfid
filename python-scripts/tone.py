8fe4573e9582c2a39ff5f12f11bc3ccfc8b8b309
Afkham Azeez <azeez@wso2.com>
2014-06-11 17:15:03 +0530

    Updated tone frequencies

--- python-scripts/tone.py
+++ python-scripts/tone.py
@@ -1,22 +1,23 @@
 import wiringpi2
 import time
 
-#wiringpi2.wiringPiSetupGpio() 
+#wiringpi2.wiringPiSetupGpio()
 wiringpi2.wiringPiSetup()
 #wiringpi2.wiringPiSetupSys()
 
 PIN=7
-FREQUENCY1=250
-FREQUENCY2=1024
+FREQUENCY1=1500
+FREQUENCY2=1600
 
 wiringpi2.softToneCreate(PIN)
 
 def playTone():
-  wiringpi2.softToneWrite(PIN,FREQUENCY1)
-  time.sleep(0.2)
-  wiringpi2.softToneWrite(PIN,FREQUENCY1)
-  time.sleep(0.1)
-  wiringpi2.softToneWrite(PIN,0)
+    wiringpi2.softToneWrite(PIN,FREQUENCY1)
+    time.sleep(0.1)
+    wiringpi2.softToneWrite(PIN,FREQUENCY2)
+    time.sleep(0.2)
+    wiringpi2.softToneWrite(PIN,0)
 
 #while True:
 #  playTone()
+#  time.sleep(1)
